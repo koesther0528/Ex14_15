2072a6b5b0667967e42aee1955a4cdb327a4cec2
esthe <ko990727@hmail.com>
2021-07-22 14:32:40 +0900

    Commit

--- app/src/main/java/kr/hs/emirim/ko502804/ex14_15/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ko502804/ex14_15/MainActivity.java
@@ -1,8 +1,11 @@
 package kr.hs.emirim.ko502804.ex14_15;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnDialog = findViewById(R.id.btn_dilog);
+        btnDialog.setOnClickListener(btnListener);
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(getApplicationContext());
+            dialog.setTitle(R.string.dilog_title);
+            dialog.setMessage(R.string.dilog_message);
+            dialog.setIcon(R.drawable.p1);
+            dialog.setPositiveButton(R.string.btn_alert, null);
+            dialog.show();
+        }
+    };
 }
